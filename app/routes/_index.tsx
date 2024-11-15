6424c417d19f91c373307e575bfe620e0b52e154
Martin Widmann <martin.widmann@russmedia.com>
2024-11-15 20:25:44 +0100

    data loading

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,10 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { SignedIn, SignedOut } from "@clerk/remix";
 import LoginLogout from "~/components/auth/LoginLogout";
 import Home from "~/components/views/Home";
 import Dashboard from "~/components/views/Dashboard";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,7 +13,22 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader: LoaderFunction = (args) => {
+  return rootAuthLoader(args, async ({ request }) => {
+    const { sessionId, userId, getToken } = request.auth;
+    // fetch data
+    console.log({
+      sessionId,
+      userId,
+      getToken: await getToken(),
+    });
+    return { yourData: "here" };
+  });
+};
+
 export default function Index() {
+  const data = useLoaderData();
+
   return (
     <div className="min-h-screen">
       <header className="flex items-center justify-between p-4 border-b border-slate-500 shadow-md bg-slate-100">
@@ -28,6 +45,7 @@ export default function Index() {
             <SignedIn>
               <Dashboard />
             </SignedIn>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
           </div>
         </div>
       </main>
